6d1f4a1ef56803d3d073f988153a149f979043fa
Rafael Gottardi <rafael.gottardi@c6bank.com>
2020-06-23 18:06:30 -0300

    A lot of funny things

--- cmd/simple-server/main.go
+++ cmd/simple-server/main.go
@@ -9,6 +9,8 @@ import (
 	"os"
 	"strings"
 	"time"
+
+	"github.com/rafaelfino/metrics"
 )
 
 //Structs:
@@ -19,10 +21,20 @@ type requestPar struct {
 }
 
 type Config struct {
-	Paths map[string]string
+	Paths      []ItemFile
+	MetricPath string
 }
 
-var cache map[string]string
+type ItemFile struct {
+	HttpPath string
+	FilePath string
+	Static   bool
+	Data     string
+}
+
+var config Config
+var cache map[string]ItemFile
+var metricProcessor *metrics.Processor
 
 //Main
 func main() {
@@ -31,12 +43,15 @@ func main() {
 		return
 	}
 
+	metricProcessor = metrics.NewMetricProcessor(time.Second*5, logExport)
+
 	loadConfig(os.Args[1])
 
 	for p, _ := range cache {
 		http.HandleFunc(p, handle)
 	}
 
+	http.HandleFunc("/refresh", handleRefresh)
 	http.HandleFunc("/echo", handleEcho)
 
 	log.Fatal(http.ListenAndServe(os.Args[2], nil))
@@ -48,14 +63,28 @@ func handle(w http.ResponseWriter, r *http.Request) {
 
 	path := getPath(r.RequestURI)
 
-	if ret, found := cache[path]; found {
-		fmt.Fprintln(w, ret)
+	if item, found := cache[path]; found {
+		if item.Static {
+			fmt.Fprintln(w, item.Data)
+		} else {
+			raw, err := ioutil.ReadFile(item.FilePath)
+
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Fprintln(w, string(raw))
+		}
+
 		log.Printf(`[%s] request returned in %s`, path, time.Since(start))
+		metricProcessor.Send(metrics.NewMetric(path, metrics.CounterType, nil, float64(time.Since(start).Nanoseconds())))
 	} else {
 		if path != `/favicon.ico` {
 			log.Printf(`data not found for uri %s`, path)
 		}
 	}
+
+	metricProcessor.Send(metrics.NewMetric("http-request", metrics.CounterType, nil, float64(time.Since(start).Nanoseconds())))
 }
 
 func handleEcho(w http.ResponseWriter, r *http.Request) {
@@ -75,11 +104,23 @@ func handleEcho(w http.ResponseWriter, r *http.Request) {
 	}
 
 	log.Printf(`[%s] request returned in %s`, path, time.Since(start))
+	metricProcessor.Send(metrics.NewMetric("http-request.echo", metrics.CounterType, nil, float64(time.Since(start).Nanoseconds())))
+}
+
+func handleRefresh(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+
+	path := getPath(r.RequestURI)
+
+	loadCache()
+
+	log.Printf(`[%s] reload cache in %s`, path, time.Since(start))
+	metricProcessor.Send(metrics.NewMetric("http-request.echo", metrics.CounterType, nil, float64(time.Since(start).Nanoseconds())))
 }
 
 //Internals
 func loadConfig(configPath string) {
-	cache = make(map[string]string)
+	cache = make(map[string]ItemFile)
 
 	data, err := ioutil.ReadFile(configPath)
 
@@ -87,22 +128,28 @@ func loadConfig(configPath string) {
 		panic(err)
 	}
 
-	var cfg Config
-
-	err = json.Unmarshal(data, &cfg)
+	err = json.Unmarshal(data, &config)
 
 	if err != nil {
 		panic(err)
 	}
 
-	for path, file := range cfg.Paths {
-		raw, err := ioutil.ReadFile(file)
+	loadCache()
+}
 
-		if err != nil {
-			panic(err)
+func loadCache() {
+	for _, item := range config.Paths {
+		if item.Static {
+			raw, err := ioutil.ReadFile(item.FilePath)
+
+			if err != nil {
+				panic(err)
+			}
+
+			item.Data = string(raw)
 		}
 
-		cache[path] = string(raw)
+		cache[item.HttpPath] = item
 	}
 }
 
@@ -114,4 +161,22 @@ func getPath(path string) string {
 	}
 
 	return path
+}
+
+//Export metrics
+func logExport(data *metrics.MetricData) error {
+	if len(data.Metrics) == 0 && len(data.Series) == 0 {
+		return nil
+	}
+
+	raw, err := json.MarshalIndent(data, "", "\t")
+
+	if err != nil {
+		log.Printf("fail to marshal metrics: %s\n", err)
+	} else {
+		log.Printf("Metrics: %s\n", string(raw))
+		ioutil.WriteFile(config.MetricPath, raw, 777)
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module learnops
 
 go 1.14
+
+require (
+	github.com/hashicorp/vault v1.4.2 // indirect
+	github.com/rafaelfino/metrics v0.0.0-20200622152306-76b17e805194
+)
