bfa1e6fba5001c29390bc720cb2b61a6d404fac4
Rafael Gottardi <rafael.gottardi@c6bank.com>
2020-06-23 18:31:08 -0300

    A lot of more things

--- cmd/data-creator/main.go
+++ cmd/data-creator/main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
 
+var url = `http://loripsum.net/api`
+
+func main() {
+	if len(os.Args) < 2 {
+		panic(fmt.Errorf("you need to inform output file path"))
+	}
+
+	log.Printf("openning file: %s\n", os.Args[1])
+
+	f, err := os.OpenFile(os.Args[1], os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0777)
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	for {
+		log.Println("executing request...")
+		resp, err := http.Get(url)
+		if err != nil {
+			panic(err)
+		}
+
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+
+		log.Println("writing file...")
+		if _, err = f.WriteString(string(body) + "\n"); err != nil {
+			panic(err)
+		}
+
+		log.Printf("waiting...")
+		time.Sleep(time.Second * 5)
+	}
+	log.Printf("stop!")
 }
--- makefile
+++ makefile
@@ -1,9 +1,13 @@
 default:
 	go build -o bin/simple-server -v cmd/simple-server/main.go
 	go build -o bin/data-creator -v cmd/data-creator/main.go
+	go build -o bin/get-currency -v cmd/get-currency/main.go	
 
 simple-server:
 	go build -o bin/simple-server -v cmd/simple-server/main.go
 
 data-creator:
 	go build -o bin/data-creator -v cmd/data-creator/main.go
+
+get-currency:
+	go build -o bin/get-currency -v cmd/get-currency/main.go	
