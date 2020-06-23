0c624d45747350ee80ab767f3af713e4b8cf8a0a
Rafael Gottardi <rafael.gottardi@c6bank.com>
2020-06-23 14:56:37 -0300

    Base implement

--- cmd/simple-server/main.go
+++ cmd/simple-server/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -17,8 +18,11 @@ type requestPar struct {
 	Values []string
 }
 
-var filePath string
-var cache string
+type Config struct {
+	Paths map[string]string
+}
+
+var cache map[string]string
 
 //Main
 func main() {
@@ -27,9 +31,12 @@ func main() {
 		return
 	}
 
-	filePath = os.Args[1]
+	loadConfig(os.Args[1])
 
-	http.HandleFunc("/", handle)
+	for p, _ := range cache {
+		http.HandleFunc(p, handle)
+	}
+
 	http.HandleFunc("/echo", handleEcho)
 
 	log.Fatal(http.ListenAndServe(os.Args[2], nil))
@@ -39,13 +46,21 @@ func main() {
 func handle(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
 
-	fmt.Fprintln(w, getFile(filePath))
+	path := getPath(r.RequestURI)
 
-	log.Printf(`request received on / in %s`, time.Since(start))
+	if ret, found := cache[path]; found {
+		fmt.Fprintln(w, ret)
+	} else {
+		log.Printf(`data not found for uri %s`, path)
+	}
+
+	log.Printf(`[%s] request returned in %s`, path, time.Since(start))
 }
 
 func handleEcho(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
+
+	path := getPath(r.RequestURI)
 
 	data := make([]requestPar, 0)
 	for key, value := range r.URL.Query() {
@@ -58,20 +73,44 @@ func handleEcho(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, string(raw))
 	}
 
-	log.Printf(`request received on /echo in %s`, time.Since(start))
+	log.Printf(`[%s] request returned in %s`, path, time.Since(start))
 }
 
 //Internals
-func getFile(path string) string {
-	if len(cache) == 0 {
-		data, err := ioutil.ReadFile(path)
+func loadConfig(configPath string) {
+	cache = make(map[string]string)
+
+	data, err := ioutil.ReadFile(configPath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var cfg Config
+
+	err = json.Unmarshal(data, &cfg)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for path, file := range cfg.Paths {
+		raw, err := ioutil.ReadFile(file)
 
 		if err != nil {
 			panic(err)
 		}
 
-		cache = string(data)
+		cache[path] = string(raw)
+	}
+}
+
+func getPath(path string) string {
+	idx := strings.Index(path, `?`)
+
+	if idx > 0 {
+		return string(path[0:idx])
 	}
 
-	return cache
+	return path
 }
