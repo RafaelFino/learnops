0720ccfb0d5fe67bbb8e08c9be1ba893755b661a
Rafael Gottardi <rafael.gottardi@c6bank.com>
2020-09-21 12:18:19 -0300

    more stuff

---
+++ scripts/debian-root-prepare.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+cd
+
+#update
+apt update -y
+apt install -y git vim zsh wget unzip jq telnet curl
+
+#install vim
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+#install zsh/vim fonts
+git clone https://github.com/powerline/fonts.git
+./fonts/install.sh
+
+#install exa (new LS)
+wget https://github.com/ogham/exa/releases/download/v0.9.0/exa-linux-x86_64-0.9.0.zip
+unzip exa-linux-x86_64-0.9.0.zip
+mv exa-linux-x86_64 /usr/local/bin/exa
+rm exa-linux-x86_64-0.9.0.zip
+
+#install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"agnoster\"/g' .zshrc
+echo 'alias ls="exa -hHBmgaFl --git"' >> ~/.zshrc
+echo set nu >> ~/.vim_runtime/my_configs.vim
+
+echo 'Done!'
